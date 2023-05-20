384ce8298c0085f198f93d863d994ac37e17531d
kumarlearns6767 <lkt929129@gmail.com>
2023-05-20 20:37:24 +0530

    made footer and navbar responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // To make the header/navbar sticky via javascript
 const header = document.querySelector("header");
 
@@ -8,8 +7,16 @@ window.addEventListener("scroll", function () {
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-
 // get year for footer
-let n =  new Date();
+let n = new Date();
 let yearr = n.getFullYear();
 document.getElementById("year").innerHTML = yearr;
+
+// To make navbar appear when clicked on hamburger icon
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+	menu.classList.toggle("bx-x");
+	navbar.classList.toggle("open");
+};
